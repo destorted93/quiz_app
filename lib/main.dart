ac69bc03d45fafe67a3612c519d73d638da9692a
djasha <dj.sandu.93@gmail.com>
2023-10-18 15:41:11 +0300

    challendge part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,13 @@
 import "package:flutter/material.dart";
-import 'package:window_manager/window_manager.dart';
 
-// optimize the main function
+import "package:quiz_app/start_screen.dart";
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // Must add this line.
-  // await windowManager.ensureInitialized();
-
-  // WindowOptions windowOptions = const WindowOptions(
-  //   size: Size(800, 600),
-  //   center: true,
-  //   backgroundColor: Colors.transparent,
-  //   skipTaskbar: false,
-  //   titleBarStyle: TitleBarStyle.hidden,
-  // );
-  // windowManager.waitUntilReadyToShow(windowOptions, () async {
-  //   await windowManager.show();
-  //   await windowManager.focus();
-  // });
-
-  runApp(const QuizApp());
-}
-
-class QuizApp extends StatelessWidget {
-  const QuizApp({super.key});
-
-  // var isFullScreen = false;
-
-  void startQuiz() {
-    // if (isFullScreen)
-    //   isFullScreen = false;
-    // else
-    //   isFullScreen = true;
-
-    // windowManager.setFullScreen(isFullScreen);
-  }
-
-  @override
-  Widget build(context) {
-    return MaterialApp(
+  runApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color.fromARGB(255, 125, 21, 143),
-                Color.fromARGB(255, 207, 14, 79),
-              ],
-            ),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  "assets/pictures/quiz-logo.png",
-                  width: 300,
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                const Text(
-                  "Learn Flutter the fun way! :)",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 28,
-                  ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                OutlinedButton(
-                  onPressed: startQuiz,
-                  child: const Text(
-                    "Start Quiz",
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 64, 0, 75),
-                      fontSize: 20,
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
+        body: StartScreen(),
       ),
-    );
-  }
+    ),
+  );
 }
